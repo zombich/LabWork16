2faa2cecba9cde8cfe38e264ea83fa083314e783
0109-04 <0109-04@0109-04>
2025-11-17 12:55:22 +0300

    task 2 start

---
+++ WebApi/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using DatabaseLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        // GET: api/<ProfileController>
+        [Authorize]
+        [HttpGet]
+        public ActionResult<CinemaUser> GetUserData()
+        {
+
+
+            return null;
+        }
+    }
+}
